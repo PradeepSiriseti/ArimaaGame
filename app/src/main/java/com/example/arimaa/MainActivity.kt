f4cb6b544b5cc2d29537973a9fd7af5c105913cd
Pradeep Siriseti <PradeepSiriseti@gmail.com>
2024-11-29 19:40:40 +0530

    Commit 3

--- app/src/main/java/com/example/arimaa/MainActivity.kt
+++ app/src/main/java/com/example/arimaa/MainActivity.kt
@@ -131,9 +131,3 @@ data class Piece(val symbol: String, val isGold: Boolean)
 // Initial game setup
 fun getInitialPieces(): Map<Pair<Int, Int>, Piece> {
     val pieces = mutableMapOf<Pair<Int, Int>, Piece>()
-
-@Preview(showBackground = true)
-@Composable
-fun ArimaaGamePreview() {
-    ArimaaGame()
-}
